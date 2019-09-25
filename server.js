2ab2798e99aa7ac92e4a5daca5b435f0546e934b
Ziri <ziri.benmouhoub@gmail.com>
2019-09-25 12:56:28 +0200

    Exo 3: gets name parameter and returns 'Bonjour, name!' or 'What is your name' if no param

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) { //a chaque fois qu'il recoit une requette get
   res.send('Hello World!')
 })
 
+app.get('/hello:name?', function (req, res) { //a chaque fois qu'il recoit une requette get à la racine du site, executer la function
+if (req.query.name){
+	res.send("Bonjour, "+ req.query.name +"!\n")
+} else {
+	res.send("What is your name?")
+}
+
+})
+
 app.listen(process.env.PORT || 3000, function () { //s'éxecute dès que le serveur est prêt à recevoir des requettes (écoute sur le port 3000)
   console.log('Example app listening on port 3000!')
 })
